3f4744203f24bc8595270d6f0b8a0021c8647449
Carlos W <carloswojahn@gmail.com>
2020-03-29 10:12:57 -0300

    Feat(Advanced): Advanced Functions * Validations - Done; * Tests - Done.

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -13,6 +13,16 @@ module.exports = {
     useNullAsDefault: true,
   },
 
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/test.sqlite'
+    }, 
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
+  },
   staging: {
     client: 'postgresql',
     connection: {
@@ -43,6 +53,7 @@ module.exports = {
     migrations: {
       tableName: 'knex_migrations'
     }
-  }
+  },
+  
 
 };
